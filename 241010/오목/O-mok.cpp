45695f7e494c335ffd48568a36f0f701b7a45bee
BEE2BEE2 <102303271+BEE2BEE2@users.noreply.github.com>
2024-10-10 00:38:07 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/오목/O-mok.cpp
+++ 241010/오목/O-mok.cpp
@@ -3,29 +3,35 @@ using namespace std;
 
 
 int someone_win(int pan[19][19],int i, int j){
-    if(pan[i-2][j] == 2 & pan[i-1][j] == 2 & pan[i][j] == 2 & pan[i+1][j] == 2 & pan[i+2][j] == 2){  
-        return 2;
+    if(2 <= i  &  i < 17){
+        if(pan[i-2][j] == 2 & pan[i-1][j] == 2 & pan[i][j] == 2 & pan[i+1][j] == 2 & pan[i+2][j] == 2){  
+            return 2;
+        }
+        if(pan[i-2][j] == 1 &  pan[i-1][j] ==1 &  pan[i][j] ==1 &  pan[i+1][j] ==1 &  pan[i+2][j] == 1){  
+            return 1;
+        }
     }
-    if(pan[i-2][j] == 1 &  pan[i-1][j] ==1 &  pan[i][j] ==1 &  pan[i+1][j] ==1 &  pan[i+2][j] == 1){  
-        return 1;
+    if(2 <= i & i < 17 & 2 <= j & j < 17){
+        if(pan[i-2][j-2] == 2 &pan[i-1][j-1] == 2 &pan[i][j] == 2 &pan[i+1][j+1] == 2 &pan[i+2][j+2] == 2){  
+            return 2;
+        }
+        if(pan[i-2][j-2] ==1 &  pan[i-1][j-1] ==1 &  pan[i][j] == 1 & pan[i+1][j+1] == 1 & pan[i+2][j+2] == 1){  
+            return 1;
+        }
+        if(pan[i-2][j+2] == 2 &pan[i-1][j+1] == 2 &pan[i][j] == 2 &pan[i+1][j-1] == 2 &pan[i+2][j-2] == 2){
+            return 2;
+        }
+        if(pan[i-2][j+2] ==1 &  pan[i-1][j+1] == 1 & pan[i][j] == 1 & pan[i+1][j-1] == 1 & pan[i+2][j-2] == 1){
+            return 1;
+        }
     }
-    if(pan[i-2][j-2] == 2 &pan[i-1][j-1] == 2 &pan[i][j] == 2 &pan[i+1][j+1] == 2 &pan[i+2][j+2] == 2){  
-        return 2;
-    }
-    if(pan[i-2][j-2] ==1 &  pan[i-1][j-1] ==1 &  pan[i][j] == 1 & pan[i+1][j+1] == 1 & pan[i+2][j+2] == 1){  
-        return 1;
-    }
-    if(pan[i-2][j+2] == 2 &pan[i-1][j+1] == 2 &pan[i][j] == 2 &pan[i+1][j-1] == 2 &pan[i+2][j-2] == 2){
-        return 2;
-    }
-    if(pan[i-2][j+2] ==1 &  pan[i-1][j+1] == 1 & pan[i][j] == 1 & pan[i+1][j-1] == 1 & pan[i+2][j-2] == 1){
-        return 1;
-    }
-    if(pan[i][j-2] == 2 &pan[i][j-1] ==2 & pan[i][j] == 2 &pan[i][j+1] == 2 &pan[i][j+2] == 2){
-        return 2;
-    }
-    if(pan[i][j-2] ==1 &  pan[i][j-1] == 1 & pan[i][j] == 1 & pan[i][j+1] ==1 &  pan[i][j+2] == 1){
-        return 1;
+    if(2 <= j  &  j < 17){
+        if(pan[i][j-2] == 2 &pan[i][j-1] ==2 & pan[i][j] == 2 &pan[i][j+1] == 2 &pan[i][j+2] == 2){
+            return 2;
+        }
+        if(pan[i][j-2] ==1 &  pan[i][j-1] == 1 & pan[i][j] == 1 & pan[i][j+1] ==1 &  pan[i][j+2] == 1){
+            return 1;
+        }
     }
     return 0;
 }
@@ -50,8 +56,8 @@ int main() {
         }
     }
     int win_flag = 0;
-    for(int i = 2; i < 17; i++){
-        for(int j = 2; j < 17; j++){
+    for(int i = 2; i < 19; i++){
+        for(int j = 2; j < 19; j++){
             int flag = someone_win(pan,i,j);
             if(flag != 0){
                 cout << flag << endl;
