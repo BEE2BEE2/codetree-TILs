78a7adc17fd0e88a9dc3305f57efc4f545b37c2d
BEE2BEE2 <102303271+BEE2BEE2@users.noreply.github.com>
2024-10-09 17:19:30 +0900

    Title: 모이자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241009/모이자/gather.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+int n = 5;
+int arr[5] = {1, 5, 2, 6, 8};
+
+int main() {
+    int max_sum = 0;
+    for(int i = 0; i < n; i++) {
+        arr[i] *= 2;
+    
+        int sum_diff = 0;
+        for(int j = 0; j < n - 1; j++)
+            sum_diff += abs(arr[j + 1] - arr[j]);
+    
+        max_sum = max(max_sum, sum_diff);
+        arr[i] /= 2;
+    }
+    
+    cout << max_sum;
+    
+    return 0;
+}
