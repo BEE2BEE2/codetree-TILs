2b68e0c3fe85a070456065b4a859321d71707fde
BEE2BEE2 <102303271+BEE2BEE2@users.noreply.github.com>
2024-10-10 00:30:03 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241010/오목/O-mok.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+
+int someone_win(int pan[19][19],int i, int j){
+    if(pan[i-2][j] == 2 & pan[i-1][j] == 2 & pan[i][j] == 2 & pan[i+1][j] == 2 & pan[i+2][j] == 2){  
+        return 2;
+    }
+    if(pan[i-2][j] == 1 &  pan[i-1][j] ==1 &  pan[i][j] ==1 &  pan[i+1][j] ==1 &  pan[i+2][j] == 1){  
+        return 1;
+    }
+    if(pan[i-2][j-2] == 2 &pan[i-1][j-1] == 2 &pan[i][j] == 2 &pan[i+1][j+1] == 2 &pan[i+2][j+2] == 2){  
+        return 2;
+    }
+    if(pan[i-2][j-2] ==1 &  pan[i-1][j-1] ==1 &  pan[i][j] == 1 & pan[i+1][j+1] == 1 & pan[i+2][j+2] == 1){  
+        return 1;
+    }
+    if(pan[i-2][j+2] == 2 &pan[i-1][j+1] == 2 &pan[i][j] == 2 &pan[i+1][j-1] == 2 &pan[i+2][j-2] == 2){
+        return 2;
+    }
+    if(pan[i-2][j+2] ==1 &  pan[i-1][j+1] == 1 & pan[i][j] == 1 & pan[i+1][j-1] == 1 & pan[i+2][j-2] == 1){
+        return 1;
+    }
+    if(pan[i][j-2] == 2 &pan[i][j-1] ==2 & pan[i][j] == 2 &pan[i][j+1] == 2 &pan[i][j+2] == 2){
+        return 2;
+    }
+    if(pan[i][j-2] ==1 &  pan[i][j-1] == 1 & pan[i][j] == 1 & pan[i][j+1] ==1 &  pan[i][j+2] == 1){
+        return 1;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+    int pan[19][19];
+
+    for(int i = 0; i < 19; i++){
+        for(int j = 0; j < 19; j++){
+            cin >> pan[i][j];
+        }
+    }
+
+    for(int i = 2; i < 17; i++){
+        for(int j = 2; j < 17; j++){
+            int flag = someone_win(pan,i,j);
+            if(flag != 0){
+                cout << flag << endl;
+                cout << i+1<< " " << j+1<< endl;
+            }
+        }
+    }
+    return 0;
+}
